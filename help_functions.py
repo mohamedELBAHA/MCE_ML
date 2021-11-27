5df5bb3f986a953df81db418e5937e0dad15086f
Ismail El Hadrami <ismail.el-hadrami@imt-atlantique.net>
2021-11-28 00:25:30 +0100

    Update help_functions.py [add import_data()]

--- help_functions.py
+++ help_functions.py
@@ -3,6 +3,7 @@
 
 import tensorflow as tf
 from tensorflow import keras
+import pandas as pd
 
 DESIRED_ACCURACY = 0.9899
 
@@ -14,3 +15,15 @@ class myCallback(tf.keras.callbacks.Callback):
         if logs.get('accuracy') is not None and logs.get('accuracy') > DESIRED_ACCURACY:
             print("\nReached 99.9% accuracy, so cancelling training!")
             self.model.stop_training = True
+
+
+# this function will import the dataset based on its name
+# coded by : Ismail El Hadrami (@i19elhad)
+
+def import_dataset(name):   
+        if name == "kidney":
+            df = pd.read_csv("kidney_disease.csv")
+            return df
+        elif name == "banknote":
+            df = pd.read_csv("data_banknote_authentication.csv")
+            return df
