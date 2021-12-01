b7feff83b5f1f2f6f963e25bc20b857231adcb9c
Mohamed El Baha <mohamed.elbaha@outlook.fr>
2021-12-01 17:42:29 +0100

    add clean_data function, change structure

--- helper_functions.py
+++ helper_functions.py
@@ -3,16 +3,64 @@
 
 
 import pandas as pd
+import numpy as np
 
 
-# this function will import the dataset based on its name
-# coded by : Ismail El Hadrami (@i19elhad)
+# this function will 
+# coded by :  (@i19elhad)
 
-def import_dataset(name):   
-        if name == "kidney":
-            df = pd.read_csv("/Data/kidney_disease.csv")
-            return df
-        elif name == "banknote":
-            df = pd.read_csv("/Data/data_banknote_authentication.csv")
-            return df
+def import_dataset(name):
+    """
+    @author : Ismail El Hadrami
+    import the data  based on its name ( kidney, banknote ...)
+
+    input : 
+        name : the name of the dataset
+        type : string
+
+    output : 
+        df   : dataset 
+        type : dataframe
+    """   
+       
+    if name == "kidney":
+        df = pd.read_csv("Data/kidney_disease.csv")
+        return df
+    elif name == "banknote":
+        df = pd.read_csv("Data/data_banknote_authentication.csv")
+        return df
+
+
+def clean_data(data):
+    """
+    @author : Mohamed EL BAHA
+    Cleaning the data provided by the replacing the uncorrect values 
+
+    input :
+        data  : dataset to be cleaned 
+        type  : DataFrame
+
+    Output :
+        data  : Cleand dataset
+        type  : DataFrame    
+    """
+
+    
+    # 1 . First we drop the index column as it's matches with the indexing 
+    # the dataframe and it's not relevant in the processing step.
+    if 'id' in data.columns : 
+        df = data.drop(columns=['id'])
+
+
+    # 2. Second change unwanted types of data (?)
+    types = ['float32', 'float64', 'float', 'int32', 'int64', 'int']
+
+    for column in data.columns : 
+        if data[column].dtype not in types :
+            data[column] = data[column].str.replace('\t','')
+            data[column] = data[column].replace('?',np.nan)
+            data[column] = data[column].str.replace(' ','')
+
+
+    return data
 
