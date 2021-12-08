41a9455773b1ee7e3f5f87ffc56a39c207d801f9
Mohamed El Baha <mohamed.elbaha@outlook.fr>
2021-12-08 20:36:44 +0100

    complete feature selection, construct pipeline

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 # Here is the main file were models shall be executed
 
 # import libaries
-import helper_functions
+import pandas as pd
 import numpy as np
-from sklearn import svm
+import helper_functions as hf
+
+
+# 1. Import Data
+# Choose the data set by typing it's name, data sets available are the banknote data set and kidney disease data set :
+# for the banknote data set please enter ==> 'banknote'
+# for the kidney diseas data set please enter ==> 'kidney'
+print("Please enter the name of the Dataset. \n for the banknote data set please enter ==> banknote \n for the kidney diseas data set please enter ==> kidney ")
+name = input()
+df = hf.import_dataset(name)
+print(" ✓ The Banknote data set succefuly imported !")
+# 2. Preprocess Data ( clean and normalize data)
+df = hf.normalize_data(df)
+print(" ✓ Data succefuly pre-prossesed ")
+
+# 3. Feature Selection  
+df = hf.feature_selection(df,'PCA',variance_threshold=0.95)
+
+# 4. Split Data ( 70% train, 30% test )
+X = df.drop(columns=['class '])
+y = df['class ']
+X_train, X_test, y_train, y_test = hf.split_data(X,y,test_size=0.3)
+
+# Explore data shapes 
+X_train.shape
+X_test.shape 
+print(f'Shape of Data \n Training Data = {X_train.shape}   | Test Data = {X_test.shape} \n Training Labels = {y_train.shape}   |  Test_labels = {y_test.shape} \n ')
+
+# 
